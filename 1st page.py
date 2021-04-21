179651053c6967a0ffd4b4da6636a9775a6f9775
SuneelGattupalli <82883756+SuneelGattupalli@users.noreply.github.com>
2021-04-21 13:14:44 +0530

    Add files via upload

---
+++ 1st page.py
@@ -0,0 +1,80 @@
+#1.a
+#class in python
+logical entity which is virtual in nature
+class.finction()
+#objects
+physica entity which is real in nature
+objects.function() 
+#methods
+methods also nothing but functions.it declared inside the class and acesss the class name only
+methos.functions()
+
+#2.A
+print("Suneel gattupalli")
+
+#3.A
+#single line comment
+print("i am suneel .i done my graduvation in vikaramasimhapuri university")
+#multiplline 
+print("""i am suneel .
+      i done my graduvation in vikaramasimhapuri university""")
+#documentation comment
+print('''i am suneel .
+      i done my graduvation in vikaramasimhapuri university''')
+#4.A
+#int
+all positive and negative non decimal numbers along with zeros
+
+#float
+all positive and negative decimal nummbes ,which are measured
+
+#complex
+numbers with real and imaginary pars
+print()
+#bool
+it defined as true or False
+a = 10
+b = 20
+print("sum = ",a+b)
+print("Subtraction = ",50-90)
+print(1.5 + 2.5)
+print(3-2j)
+print("true")
+
+#5.A
+Local variables can only be reached within their scope(like func() above).
+Like in below program- there are two local variables – x and y.
+
+def sum(x,y):
+sum = x + y
+return sum
+print(sum(5, 10))
+
+#global variable
+A global variable can be used anywhere in the program as its scope is the entire program.
+Let’s understand global variable with a very simple example -
+
+def func(x, y):
+global a
+def func():
+global z
+print(z)
+z=20
+func()
+print(z)
+# Write a function to print your name and call the function from main method
+functions = {'myfoo': foo.bar}
+
+mystring = 'myfoo'
+if mystring in functions:
+    functions[mystring]()
+
+
+#methoids
+
+
+
+
+
+
+#
